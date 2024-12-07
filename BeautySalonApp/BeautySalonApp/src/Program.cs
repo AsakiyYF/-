fb0b930b7b7d7ae55ed4d39e2a0123da829a9207
AsakiyYF <149182328+AsakiyYF@users.noreply.github.com>
2024-12-07 11:23:57 +0400

    Add files via upload

---
+++ BeautySalonApp/BeautySalonApp/src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BeautySalonApp
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            //посхалка отсылка хзз
+        }
+    }
+}
---
+++ BeautySalonApp/BeautySalonApp/src/Services/AppointmentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautySalonApp.src.Services
+{
+    internal class AppointmentService
+    {
+    }
+}
